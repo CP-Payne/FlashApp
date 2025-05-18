98d164ec2a189ddaf85c5a7a8963a8083d83f716
Charles <charlethical1@gmail.com>
2025-05-18 07:26:39 +0200

    style: remove status comments from IFlashcardRepository

--- Interfaces/Repository/IFlashcardRepository.cs
+++ Interfaces/Repository/IFlashcardRepository.cs
@@ -8,23 +8,12 @@ namespace FlashApp.Interfaces.Repository
 {
     public interface IFlashcardRepository
     {
-        // Completed
         Task<List<Flashcard>> GetAllByUserIdAsync(string userId);
-
-        // Completed
         Task<Flashcard?> GetByIdAndUserIdAsync(Guid flashcardId, string userId);
-
-        // Completed
         Task<Flashcard?> GetByQuestionAndUserIdAsync(string question, string userId);
-
-        // Completed
         Task AddAsync(Flashcard flashcard);
-
-        // Completed
         void Update(Flashcard flashcard);
         void Delete(Flashcard flashcard);
-
-        // SaveChangesAsync();
         Task<bool> SaveChangesAsync();
     }
 }
